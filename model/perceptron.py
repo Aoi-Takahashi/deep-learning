7355d380c06a520c68d4e1510f4cf7c843e3e90b
Aoi-Takahashi <aoi.stc.tw.0213@gmail.com>
2026-03-25 01:56:31 +0900

    implement OR, NAND, XOR

--- model/perceptron.py
+++ model/perceptron.py
@@ -1,9 +1,41 @@
+import numpy as np
+
+
 def AND(x1, x2):
-    w1, w2, theta = 0.5, 0.5, 0.7
-    y = w1 * x1 + w2 * x2
-    if y <= theta:
-        print(f"Not Fire y is {y}")
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.7
+    y = np.sum(w * x) + b
+    if y <= 0:
         return 0
     else:
-        print(f"Fire! y is {y}")
         return 1
+
+
+def OR(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.2
+    y = np.sum(w * x) + b
+    if y <= 0:
+        return 0
+    else:
+        return 1
+
+
+def NAND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([-0.5, -0.5])
+    b = 0.7
+    y = np.sum(w * x) + b
+    if y <= 0:
+        return 0
+    else:
+        return 1
+
+
+def XOR(x1, x2):
+    s1 = NAND(x1, x2)
+    s2 = OR(x1, x2)
+    y = AND(s1, s2)
+    return y
--- tests/test_perceptron.py
+++ tests/test_perceptron.py
@@ -1,17 +1,81 @@
-from model.perceptron import AND
+from model.perceptron import AND, NAND, OR, XOR
 
 
-def test_AND_0_0():
+# (x1,x2)=(0,0)
+def test_AND1():
     assert AND(0, 0) == 0
 
 
-def test_AND_1_0():
+# (x1,x2)=(1,0)
+def test_AND2():
     assert AND(1, 0) == 0
 
 
-def test_AND_0_1():
+# (x1,x2)=(0,1)
+def test_AND3():
     assert AND(0, 1) == 0
 
 
-def test_AND_1_1():
+# (x1,x2)=(1,1)
+def test_AND4():
     assert AND(1, 1) == 1
+
+
+# (x1,x2)=(0,0)
+def test_OR1():
+    assert OR(0, 0) == 0
+
+
+# (x1,x2)=(1,0)
+def test_OR2():
+    assert OR(1, 0) == 1
+
+
+# (x1,x2)=(0,1)
+def test_OR3():
+    assert OR(0, 1) == 1
+
+
+# (x1,x2)=(1,1)
+def test_OR4():
+    assert OR(1, 1) == 1
+
+
+# (x1,x2)=(0,0)
+def test_NAND1():
+    assert NAND(0, 0) == 1
+
+
+# (x1,x2)=(1,0)
+def test_NAND2():
+    assert NAND(1, 0) == 1
+
+
+# (x1,x2)=(0,1)
+def test_NAND3():
+    assert NAND(0, 1) == 1
+
+
+# (x1,x2)=(1,1)
+def test_NAND4():
+    assert NAND(1, 1) == 0
+
+
+# (x1,x2)=(0,0)
+def test_XOR1():
+    assert XOR(0, 0) == 0
+
+
+# (x1,x2)=(1,0)
+def test_XOR2():
+    assert XOR(1, 0) == 1
+
+
+# (x1,x2)=(0,1)
+def test_XOR3():
+    assert XOR(0, 1) == 1
+
+
+# (x1,x2)=(1,1)
+def test_XOR4():
+    assert XOR(1, 1) == 0
