bb18f7d0b40bd9d21d1014e2a4f0a55caaeeb11a
Aoi-Takahashi <aoi.stc.tw.0213@gmail.com>
2026-04-04 22:36:33 +0900

    feat: Sigmoid関数の実装

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
-from model.activation import step_function
+from model.activation import sigmoid
 
 
 def main():
     x = np.arange(-5.0, 5.0, 0.1)
-    y = step_function(x)
+    y = sigmoid(x)
+    # y = step_function(x)
     plt.plot(x, y)
     plt.ylim(-0.1, 1.1)  # y軸の範囲を指定
-    plt.savefig("figure/Step関数.png")
+    # plt.savefig("figure/Step関数.png")
+    plt.savefig("figure/sigmoid.png")
 
 
 if __name__ == "__main__":
--- model/activation.py
+++ model/activation.py
@@ -4,3 +4,8 @@ import numpy as np
 # Step関数
 def step_function(x):
     return np.array(x > 0, dtype=int)
+
+
+# Sigmoid関数
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
