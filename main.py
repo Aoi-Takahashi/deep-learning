330f886af75709ed9777391b4825484407731c4f
Aoi-Takahashi <aoi.stc.tw.0213@gmail.com>
2026-06-25 21:18:40 +0900

    feat: main関数の修正

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 import numpy as np
-from matplotlib import pyplot as plt
 
-from model.activation import relu
+# from matplotlib import pyplot as plt
+
+# from model.activation import relu
 
 
 def main():
-    x = np.arange(-5.0, 5.0, 0.1)
+    B = np.array([[1, 2], [3, 4], [5, 6]])
+    print(B)
+    print(np.ndim(B))
+    print(np.shape(B))
+    # x = np.arange(-5.0, 5.0, 0.1)
     # y = step_function(x)
     # y = sigmoid(x)
-    y = relu(x)
-    plt.plot(x, y)
-    plt.ylim(-0.1, 1.1)  # y軸の範囲を指定
+    # y = relu(x)
+    # plt.plot(x, y)
+    # plt.ylim(-0.1, 1.1)  # y軸の範囲を指定
     # plt.savefig("figure/Step関数.png")
     # plt.savefig("figure/sigmoid.png")
-    plt.savefig("figure/relu.png")
+    # plt.savefig("figure/relu.png")
 
 
 if __name__ == "__main__":
