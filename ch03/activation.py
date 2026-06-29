8bdff918c64a36f67528091fcd02045d15268cbd
Aoi-Takahashi <aoi.stc.tw.0213@gmail.com>
2026-06-29 23:24:49 +0900

    feat: ニt: ニューラルネットワーク（MNIST）の実装

--- ch03/activation.py
+++ ch03/activation.py
@@ -14,3 +14,9 @@ def sigmoid(x):
 # ReLU関数
 def relu(x):
     return np.maximum(0, x)
+
+
+# Softmax関数
+def softmax(x):
+    x = x - np.max(x, axis=-1, keepdims=True)  # オーバーフロー対策
+    return np.exp(x) / np.sum(np.exp(x), axis=-1, keepdims=True)
