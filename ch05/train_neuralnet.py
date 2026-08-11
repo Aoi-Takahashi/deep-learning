ab6591767af3437940200552be5fb551c6f941cf
Aoi-Takahashi <aoi.stc.tw.0213@gmail.com>
2026-08-11 20:55:44 +0900

    fix: unresolved import path

--- ch05/train_neuralnet.py
+++ ch05/train_neuralnet.py
@@ -2,13 +2,13 @@
 import os
 import sys
 
-sys.path.append(os.pardir)
+sys.path.append(os.path.join(os.path.dirname(__file__), os.pardir))
 
 import numpy as np
 
 from dataset.mnist import load_mnist
 
-from .two_layer_net import TwoLayerNet
+from two_layer_net import TwoLayerNet
 
 # データの読み込み
 (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
